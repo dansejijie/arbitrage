d7ab9afc906903fa79db2d10456206a4727eb805
zheng qiang <dansejijie@163.com>
2018-03-25 22:25:59 +0800

    'carry_kline'

--- kline_carry/main.py
+++ kline_carry/main.py
@@ -9,23 +9,7 @@ from market.Okex import Okex
 from market.Zb import Zb
 from market.HuoBi import HuoBi
 from bmob.Bmob import Bmob
-
-"""
-获取K线
-"""
-def getKLines(symbol_setting,period):
-  klines=[]
-  for market in market_symbol:
-    if symbol_setting[market]==True:
-        klines.append(markets[market].kline(symbol_setting["symbol"],period))
-  return klines
-
-def getSameLines(Klines,start_date,end_date):
-  newklines=[]
-  for i in range(len(klines)):
-    newklines.append(getSameLine(klines[i],start_date,end_date))
-  return newklines
-
+from utils.MessageSender import MessageSender
 """
 剪切两边，补充缺失K线
 """
@@ -48,82 +32,78 @@ def getSameLine(kline,start_date,end_date):
             newKline_array.append(k)
   kline.setInfo(kline.symbol,newKline_array)
   return kline
-        
-def getStartAndEndDate(klines):
-  max_start_date=0
-  min_end_date=float("inf")
-  for i in range(len(klines)):
-    if klines[i][0].date>max_start_date:
-      max_start_date=klines[i][0].date
-    if klines[i][-1].date<min_end_date:
-      min_end_date=klines[i][-1].date
-  return max_start_date,min_end_date
 
-def judgeklines(klines):
-  medium=Medium()
-  for i in range(len(klines)):
-    if klines[i].result==False:
-      medium.setError(klines[i].message)
-      break
-  return medium
+
   
-def transBmobKline(klines):
-  newKLines=[]
-  i=0
-  while True:
-    try:
-      temp={}
-      for j in range(len(klines)):
-        temp["date"]=klines[j][i].date
-        temp["{}_open".format(klines[j].market)]=klines[j][i].open
-        temp["{}_high".format(klines[j].market)]=klines[j][i].high
-        temp["{}_low".format(klines[j].market)]=klines[j][i].low
-        temp["{}_close".format(klines[j].market)]=klines[j][i].close
-        temp["{}_vol".format(klines[j].market)]=klines[j][i].vol
-      newKLines.append(temp)
-      i+=1
-    except Exception as e:
-      break
-  return newKLines
+def transBmobKline(kline):
+  newKLine=[]
+  for i in range(len(kline)):
+    temp={}
+    temp["date"]=kline[i].date
+    temp["open"]=kline[i].open
+    temp["high"]=kline[i].high
+    temp["low"]=kline[i].low
+    temp["close"]=kline[i].close
+    temp["vol"]=kline[i].vol
+    newKLine.append(temp)
+  return newKLine
 
 def startHandleEvent():
-
-  for symbol_setting in coins_settings:
-    print('开始处理{}'.format(symbol_setting["symbol"]))
+  for coin in coins:
+    print('开始处理{}'.format(coin))
     search_date=None
-    result=bmob.queryArrayByLimit(symbol_setting["symbol"],"-date",1,0)
+    result=bmob.queryArrayByLimit(coin,"-date",1,0)
     if len(result.extra)>0:
       search_date=result.extra[0]["date"]
-    klines=getKLines(symbol_setting,"15min")
-    result=judgeklines(klines)
-    if result.result==True:
-      start_date,end_date=getStartAndEndDate(klines)
+    kline=market.kline(coin,"15min")
+    if kline.result==True:
+      start_date=kline[0].date
+      end_date=kline[-1500].date
       if search_date!=None and search_date>start_date:
         start_date=search_date
-      klines=getSameLines(klines,start_date,end_date)
-      newLines=transBmobKline(klines)
-      print('{}正在上传Bmob数据库中'.format(symbol_setting["symbol"]))
-      result=bmob.insertArray(symbol_setting["symbol"],newLines)
+      kline=getSameLine(kline,start_date,end_date)
+      newLine=transBmobKline(kline)
+      print('{}正在上传Bmob数据库中'.format(coin))
+      result=bmob.insertArray(coin,newLine)
       if result.result==False:
-        print('{}上传到Bmob数据库发生失败{}'.format(symbol_setting["symbol"],result.message))
+        print('{}上传到Bmob数据库发生失败{}'.format(coin,result.message))
+        return error_interval_time
+      else:
+        with open(os.path.join(os.path.dirname(__file__),'info.txt'),"a") as f:
+          f.write("{}:{} last date:{} today date:{} num:{}\n".format(time.strftime('%Y-%m-%d %H:%M:%S'),coin,search_date,start_date,len(kline)))
     else:
       print(result.message)
+      return error_interval_time
+  return correct_interval_time
 
+
+#kline 可以获取2000根 因此 correct_interval_time=1080000 error_interval_time=270000 最好
 if __name__ == '__main__':
   
+    correct_interval_time=86400
+    error_interval_time=30000
+    error_num=0
+
     with open(os.path.join(os.path.dirname(__file__),os.path.pardir,'config.json'),'r') as f:
       config=json.load(f)
-    bmob=Bmob(config["bmob"]["coin"]["api_key"],config["bmob"]["coin"]["secret_key"])
-    market_symbol=["okex","huobi","zb"]
-    markets={"okex":Okex('xx','xx'),"huobi":HuoBi('xx','xx'),"zb":Zb('xx','xx')}
-    
+    bmob=Bmob(config["bmob"]["api_key"],config["bmob"]["secret_key"])
+    market=Okex('xx','xx')
+    messageSender=MessageSender(config["message"]["username"],config["message"]["password"])
     coin_path=os.path.join(os.path.dirname(__file__),'coins.json')
     with open(coin_path,'rt') as f:
-      coins_settings=json.load(f)
+      coins=json.load(f)
 
-    while True:
-      startHandleEvent()
-      time.sleep(1810)
-      print('程序等待下一次执行中')
+    try:
+      while True:
+        interval_time=startHandleEvent()
+        if interval_time==error_interval_time:
+          error_num+=1
+          if error_num>12:
+              error_num=0
+              messageSender.sendText("错误次数太多，请尽快查看原因")
+        print('程序等待下一次执行中')
+        time.sleep(interval_time)
+        
+    except Exception as e:
+      messageSender.sendText(e.args[0])
     print('程序退出循环')
-    #klines=getKLines(symbol,"15min")
